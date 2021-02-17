77b47090bf1b7e1eebcf0c100c667cf426f91c5e
Javier Lecuona <javierlecuona@gmail.com>
2021-02-17 18:20:56 -0300

    prioritizing go types when sizes are the same

--- impack.go
+++ impack.go
@@ -17,6 +17,29 @@ import (
 	"golang.org/x/tools/go/packages"
 )
 
+const (
+	emptyInterface   = "interface{}"
+	goTypeBool       = "bool"
+	goTypeString     = "string"
+	goTypeInt        = "int"
+	goTypeInt8       = "int8"
+	goTypeInt16      = "int16"
+	goTypeInt32      = "int32"
+	goTypeInt64      = "int64"
+	goTypeUint       = "uint"
+	goTypeUint8      = "uint8"
+	goTypeUint16     = "uint16"
+	goTypeUint32     = "uint32"
+	goTypeUint64     = "uint64"
+	goTypeUintptr    = "uintptr"
+	goTypeByte       = "byte"
+	goTypeRune       = "rune"
+	goTypeFloat32    = "float32"
+	goTypeFloat64    = "float64"
+	goTypeComplex64  = "complex64"
+	goTypeComplex128 = "complex128"
+)
+
 // Linter performs a simple memory packing on go a package
 type Linter struct {
 	sizes types.Sizes
@@ -86,9 +109,13 @@ func (linter *Linter) Lint(ctx context.Context, projectRoot string) error {
 func (linter *Linter) lintStruct(st *dst.StructType, typesStruct *types.Struct) error {
 	nf := typesStruct.NumFields()
 	fieldSizes := make(map[string]int64, nf)
+	fieldTypes := make(map[string]string, nf)
 	for i := 0; i < nf; i++ {
 		f := typesStruct.Field(i)
-		fieldSizes[f.Name()] = linter.typeSize(f.Type())
+		fieldName := f.Name()
+		fieldType := f.Type()
+		fieldSizes[fieldName] = linter.typeSize(fieldType)
+		fieldTypes[fieldName] = fieldType.String()
 	}
 	sort.SliceStable(st.Fields.List, func(i, j int) bool {
 		field1 := st.Fields.List[i]
@@ -98,13 +125,22 @@ func (linter *Linter) lintStruct(st *dst.StructType, typesStruct *types.Struct) 
 		size1 := fieldSizes[name1]
 		size2 := fieldSizes[name2]
 		if size1 == size2 {
-			// if sizes are equal then order alphabetically
-			var name1Lower = strings.ToLower(name1)
-			var name2Lower = strings.ToLower(name2)
-			if name1Lower == name2Lower {
-				return name1 < name2
+			type1 := fieldTypes[name1]
+			type2 := fieldTypes[name2]
+			// if sizes is equal but type is different then order by type alphabetically
+			if type1 != type2 {
+				if isGoType(type1) && isGoType(type2) {
+					// if both types are go types then order by type alphabetically
+					return orderAlpha(type1, type2)
+				} else if isGoType(type1) && !isGoType(type2) {
+					// prioritize go types first
+					return true
+				} else {
+					return false
+				}
 			}
-			return name1Lower < name2Lower
+			// if sizes and type are equal then order by field name alphabetically
+			return orderAlpha(name1, name2)
 		}
 		// lower sizes on top
 		return size1 < size2
@@ -125,6 +161,15 @@ func (linter *Linter) typeSize(t types.Type) int64 {
 		return align(size, alignment)*(n-1) + size
 	}
 	return linter.sizes.Sizeof(t)
+}
+
+func orderAlpha(field1, field2 string) bool {
+	field1Lower := strings.ToLower(field1)
+	field2Lower := strings.ToLower(field2)
+	if field1Lower == field2Lower {
+		return field1 < field2
+	}
+	return field1Lower < field2Lower
 }
 
 func align(x int64, a int64) int64 {
@@ -152,4 +197,51 @@ func writeFile(path string, dstFile *dst.File) error {
 	defer f.Close()
 	_, err = io.Copy(f, buf)
 	return err
+}
+
+func isGoType(t string) bool {
+	switch t {
+	case emptyInterface:
+		return true
+	case goTypeBool:
+		return true
+	case goTypeString:
+		return true
+	case goTypeInt:
+		return true
+	case goTypeInt8:
+		return true
+	case goTypeInt16:
+		return true
+	case goTypeInt32:
+		return true
+	case goTypeInt64:
+		return true
+	case goTypeUint:
+		return true
+	case goTypeUint8:
+		return true
+	case goTypeUint16:
+		return true
+	case goTypeUint32:
+		return true
+	case goTypeUint64:
+		return true
+	case goTypeUintptr:
+		return true
+	case goTypeByte:
+		return true
+	case goTypeRune:
+		return true
+	case goTypeFloat32:
+		return true
+	case goTypeFloat64:
+		return true
+	case goTypeComplex64:
+		return true
+	case goTypeComplex128:
+		return true
+	default:
+		return false
+	}
 }
